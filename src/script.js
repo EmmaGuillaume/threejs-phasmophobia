f832a111db1835d556814ab8da0d9eb7276e776c
Emma Guillaume <emma.guillaume@mmibordeaux.com>
2024-01-19 23:36:53 +0100

    [FEAT] - add couch model and make move Maxwell (cat), + clean file

--- src/script.js
+++ src/script.js
@@ -1,51 +1,26 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import GUI from 'lil-gui'
-import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
-import { FirstPersonControls } from 'three/addons/controls/FirstPersonControls.js';
+
 
 
 
 const gui = new GUI()
-
 const canvas = document.querySelector('canvas.webgl')
-
 const scene = new THREE.Scene()
 
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.015)
+
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.009)
 
 scene.add(ambientLight)
 
-
-gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
-
-
-const textureLoader = new THREE.TextureLoader()
-const test = textureLoader.load('textures/matcaps/8.png')
-
-// const spotLight = new THREE.SpotLight('#FFFDF3', 4.5, 20, Math.PI * 0.1, 0.25, 1)
-// spotLight.position.set(5, 2, 5)
-// spotLight.lookAt(new THREE.Vector3())
+// GUI
+// gui.add(ambientLight, 'intensity').min(0).max(3).step(0.001)
 
 
-
-
-
-
-// scene.add(spotLight)
-
-
-// const spotLightHelper = new THREE.SpotLightHelper(spotLight)
-// scene.add(spotLightHelper)
-
-
-
+// MODELES
 const loader = new GLTFLoader();
-
-
-
 loader.load( 'models/Houseplant.glb', function ( gltf ) {
 gltf.scene.scale.set(2,2,2)
 gltf.scene.position.set(-0.8,-0.64,-0.8)
@@ -76,12 +51,18 @@ loader.load( 'models/Lamp.glb', function ( gltf ) {
     gltf.scene.position.set(1.5,-2.2,-2)
     gltf.scene.rotation.set(0,3,0)
         scene.add( gltf.scene );
+        let lamp2 =  gltf.scene.clone()
+        lamp2.position.set(6,-2.2,6)
+        lamp2.rotation.set(0,-0.5,0)
+        scene.add( gltf.scene, lamp2 );
+
     
     }, undefined, function ( error ) {
     
         console.error( error );
     
     } );
+
 
 loader.load( 'models/Rug.glb', function ( gltf ) {
     gltf.scene.scale.set(20,20,20)
@@ -94,6 +75,20 @@ loader.load( 'models/Rug.glb', function ( gltf ) {
         console.error( error );
         
 } );
+
+loader.load( 'models/Couch-Small.glb', function ( gltf ) {
+    gltf.scene.scale.set(0.6,0.6,0.6)
+    gltf.scene.position.set(5,-0.65,4.5)
+    gltf.scene.rotation.set(0,1.2,0)
+
+        scene.add( gltf.scene );
+        
+    }, undefined, function ( error ) {
+        
+        console.error( error );
+        
+} );
+
 
 
 loader.load( 'models/Bed-Double.glb', function ( gltf ) {
@@ -135,11 +130,27 @@ loader.load( 'models/Watering-Can.glb', function ( gltf ) {
         console.error( error );
     
     } );
+    
 
-let maxwell = loader.load( 'models/Maxwell.glb', function ( gltf ) {
+    let maxwell;
+
+loader.load('models/Maxwell.glb', function (gltf) {
     gltf.scene.scale.set(0.35,0.35,0.35)
     gltf.scene.position.set(-1,-0.6,1.5)
     gltf.scene.rotation.set(0,1.8,0)
+    
+    scene.add(gltf.scene);
+    maxwell = gltf.scene;  
+
+}, undefined, function (error) {
+    console.error(error);
+});
+
+
+loader.load( 'models/Doorway.glb', function ( gltf ) {
+    gltf.scene.scale.set(4,4,4)
+    gltf.scene.position.set(7.2,-0.5,1)
+    gltf.scene.rotation.set(0,1.5,0)
     
     scene.add( gltf.scene );
     console.log(gltf.scene)
@@ -152,6 +163,7 @@ let maxwell = loader.load( 'models/Maxwell.glb', function ( gltf ) {
 
 
 
+//MATERIALS
 const material = new THREE.MeshStandardMaterial()
 material.roughness = 0.4
 
@@ -170,17 +182,30 @@ const wall2 = new THREE.Mesh(
     new THREE.PlaneGeometry(10, 10),
     material
 )
+const wall3 = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    material
+)
+const wall4 = new THREE.Mesh(
+    new THREE.PlaneGeometry(10, 10),
+    material
+)
 
 plane.rotation.x = - Math.PI * 0.5
 plane.position.set(2.5, -0.65, 2.5)
 wall1.position.set(2.5, 4.3, -2.5)
 wall2.position.set(-2.5, 4.3, 2.5)
 wall2.rotation.y = Math.PI * 0.5
+wall3.position.set(2.5, 4.3, 7.4)
+wall3.rotation.y = Math.PI * 1
+wall4.position.set(7.4, 4.3, 2.5)
+wall4.rotation.y = Math.PI * 1.5
 
 
+scene.add(plane, wall1, wall2, wall3, wall4)
 
-scene.add(plane, wall1, wall2)
 
+// SIZES
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -199,78 +224,39 @@ window.addEventListener('resize', () =>
 
 })
 
-
+// CAMERA
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.2, 100)
-// const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 1, 1000)
-
-camera.position.set(5, 2, 5)
-
-// camera.lookAt(new THREE.Vector3())
-
-
-
+camera.position.set(2, 4, 2)
 scene.add(camera)
 
+// TORCH
 const flashlight = new THREE.SpotLight('#FFFDEF', 4.5, 30, Math.PI * 0.1, 0.25, 1);
 flashlight.position.set(0,0,1);
 flashlight.target = camera;
 camera.add(flashlight);
 
-
+// CONTROLS CAMERA
 const controls = new OrbitControls(camera, canvas)
-// const controls = new PointerLockControls( camera, canvas );
-
-controls.enableZoom = false;
-// controls.enablePan = false;
 
 
-
-
-const cursor = {
-    x: 0,
-    y: 0
-}
-
-
-window.addEventListener('mousemove', (event) =>
-{
-    cursor.x = event.clientX / sizes.width - 0.5
-    cursor.y = - (event.clientY / sizes.height - 0.5)
-})
-/**
- * Renderer
- */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// renderer.setClearColor('#ffffff');
 
 
-/**
- * Animate
- */
-const clock = new THREE.Clock()
+let time = 0;
+const tick = () => {
+    time += 0.1; 
 
-const tick = () =>
-{
-    // const elapsedTime = clock.getElapsedTime()
-    // camera.rotation.z = Math.cos(cursor.y * Math.PI * 2) * 2
+    if (maxwell) {
+        maxwell.rotation.x = Math.sin(time); 
+    }
 
-    camera.rotation.y = Math.cos(cursor.x * Math.PI * 2) * 2
-   camera.rotation.x = Math.sin(cursor.y * Math.PI * 2) * 2
-
-    // Update controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+    controls.update();
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(tick);
 }
 
-tick()
-
-
+tick();
